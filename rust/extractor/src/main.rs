498bbc80b665c81e38c2ba8267325b0d1bf14efc
Arthur Baars <aibaars@github.com>
2024-12-18 16:07:55 +0100

    Rust: cargo clippy

--- rust/extractor/src/main.rs
+++ rust/extractor/src/main.rs
@@ -218,7 +218,7 @@ fn main() -> anyhow::Result<()> {
     let cargo_config = cfg.to_cargo_config(&cwd()?);
     for (manifest, files) in map.values().filter(|(_, files)| !files.is_empty()) {
         if let Some((ref db, ref vfs)) = extractor.load_manifest(manifest, &cargo_config) {
-            crate_graph::extract_crate_graph(&extractor.traps, db, vfs);
+            crate_graph::extract_crate_graph(extractor.traps, db, vfs);
             let semantics = Semantics::new(db);
             for file in files {
                 match extractor.load_source(file, &semantics, vfs) {
