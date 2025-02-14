b17ffd2c3f9c58cbd6cb3ddfc4395404ed2c3667
Paolo Tranquilli <redsun82@gihub.com>
2025-02-14 16:45:49 +0100

    Shared: finish fixing clippy warnings

--- shared/tree-sitter-extractor/src/diagnostics.rs
+++ shared/tree-sitter-extractor/src/diagnostics.rs
@@ -124,7 +124,7 @@ impl LogWriter {
                 match std::fs::OpenOptions::new()
                     .create(true)
                     .append(true)
-                    .write(true)
+
                     .open(path)
                 {
                     Err(e) => {
@@ -241,7 +241,7 @@ impl DiagnosticMessage {
     }
 
     fn text(&mut self, text: &str) -> &mut Self {
-        self.plaintext_message = text.to_owned();
+        text.clone_into(&mut self.plaintext_message);
         self
     }
 
@@ -286,7 +286,7 @@ impl DiagnosticMessage {
     }
 
     pub fn markdown(&mut self, text: &str) -> &mut Self {
-        self.markdown_message = text.to_owned();
+        text.clone_into(&mut self.markdown_message);
         self
     }
     pub fn severity(&mut self, severity: Severity) -> &mut Self {
